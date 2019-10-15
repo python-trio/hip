3da0d0a9f1667fdbfe5d68d50e5b2ff3302218ee
Quentin Pradet <quentin.pradet@gmail.com>
2019-10-15 11:01:21 +0400

    Restore new lines in exceptions.py
    They were incorrectly (as far as I can tell) removed by the merge
    commit.

--- src/urllib3/exceptions.py
+++ src/urllib3/exceptions.py
@@ -66,10 +66,12 @@ ConnectionError = ProtocolError
 
 class MaxRetryError(RequestError):
     """Raised when the maximum number of retries is exceeded.
+
     :param pool: The connection pool
     :type pool: :class:`~urllib3.connectionpool.HTTPConnectionPool`
     :param string url: The requested Url
     :param exceptions.Exception reason: The underlying error
+
     """
 
     def __init__(self, pool, url, reason=None):
@@ -88,6 +90,7 @@ class TimeoutStateError(HTTPError):
 
 class TimeoutError(HTTPError):
     """ Raised when a socket timeout error occurs.
+
     Catching this error will catch both :exc:`ReadTimeoutErrors
     <ReadTimeoutError>` and :exc:`ConnectTimeoutErrors <ConnectTimeoutError>`.
     """
