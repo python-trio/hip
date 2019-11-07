0da2b55140f33e38957d70752854de0d07d39a0f
Quentin Pradet <quentin.pradet@gmail.com>
2019-11-07 20:59:41 +0400

    Preserve chunked parameter on broken connections (#1734)
    Redirect are not handled at this level in hip, so we can remove one
    test.

--- test/with_dummyserver/test_chunked_transfer.py
+++ test/with_dummyserver/test_chunked_transfer.py
@@ -127,3 +127,32 @@ class TestChunkedTransfer(SocketDummyServerTestCase):
                 retries=retries,
             )
         assert self.chunked_requests == 2
+
+    def test_preserve_chunked_on_broken_connection(self):
+        self.chunked_requests = 0
+
+        def socket_handler(listener):
+            for i in range(2):
+                sock = listener.accept()[0]
+                request = consume_socket(sock)
+                if b"transfer-encoding: chunked" in request.split(b"\r\n"):
+                    self.chunked_requests += 1
+
+                if i == 0:
+                    # Bad HTTP version will trigger a connection close
+                    sock.send(b"HTTP/0.5 200 OK\r\n\r\n")
+                else:
+                    sock.send(b"HTTP/1.1 200 OK\r\n\r\n")
+                sock.close()
+
+        self._start_server(socket_handler)
+        with HTTPConnectionPool(self.host, self.port) as pool:
+            retries = Retry(read=1)
+            pool.urlopen(
+                "GET",
+                "/",
+                body=iter([b"chunk1", b"chunk2"]),
+                preload_content=False,
+                retries=retries,
+            )
+        assert self.chunked_requests == 2
