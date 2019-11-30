ab9096b9c276ea65ddf1e42acfa1712b7d98b53b
Quentin Pradet <quentin.pradet@gmail.com>
2019-11-30 07:30:16 +0400

    demo: rename urllib3 to hip

--- demo/async-demo.py
+++ demo/async-demo.py
@@ -1,32 +1,32 @@
 # This should work on python 3.6+
 
-import urllib3
-from urllib3.backends import Backend
+import hip
+from hip.backends import Backend
 
 URL = "http://httpbin.org/uuid"
 
 async def main(backend=None):
-    with urllib3.AsyncPoolManager(backend=backend) as http:
+    with hip.AsyncPoolManager(backend=backend) as http:
         print("URL:", URL)
         r = await http.request("GET", URL, preload_content=False)
         print("Status:", r.status)
         print("Data:", await r.read())
 
-print("--- urllib3 using Trio ---")
+print("--- hip using Trio ---")
 import trio
 trio.run(main)
 
-print("\n--- urllib3 using asyncio (via AnyIO) ---")
+print("\n--- hip using asyncio (via AnyIO) ---")
 import asyncio
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 loop.close()
 
-print("\n--- urllib3 using curio (via AnyIO) ---")
+print("\n--- hip using curio (via AnyIO) ---")
 import curio
 curio.run(main)
 
-print("\n--- urllib3 using Twisted ---")
+print("\n--- hip using Twisted ---")
 from twisted.internet.task import react
 from twisted.internet.defer import ensureDeferred
 def twisted_main(reactor):
--- demo/sync-demo.py
+++ demo/sync-demo.py
@@ -2,12 +2,12 @@
 
 from __future__ import print_function
 
-import urllib3
+import hip
 
 URL = "http://httpbin.org/uuid"
 
-print("--- urllib3 using synchronous sockets ---")
-with urllib3.PoolManager() as http:
+print("--- hip using synchronous sockets ---")
+with hip.PoolManager() as http:
     print("URL:", URL)
     r = http.request("GET", URL, preload_content=False)
     print("Status:", r.status)
