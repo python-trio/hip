6937f5b5b12fb56fd795dd746a87361f97d16699
Seth Michael Larson <seth.larson@elastic.co>
2020-05-06 11:57:43 -0500

    Add Trio+RTD theme and readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,57 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from datetime import date
+
 import os
 import sys
+import datetime
 
-import alabaster
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, root_path)
+sys.path.insert(0, os.path.abspath("../.."))
 
 import hip
 
+# Warn about all references to unknown targets
+nitpicky = False  # TODO: Switch this to 'True' after interface has solidified.
+nitpick_ignore = []
+autodoc_inherit_docstrings = False
+default_role = "obj"
 
-# -- General configuration -----------------------------------------------------
+# XX hack the RTD theme until
+#   https://github.com/rtfd/sphinx_rtd_theme/pull/382
+# is shipped (should be in the release after 0.2.4)
+# ...note that this has since grown to contain a bunch of other CSS hacks too
+# though.
 
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+
+def setup(app):
+    app.add_css_file("hackrtd.css")
+
+
+# -- General configuration ------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "alabaster",
     "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib_trio",
 ]
 
-# Test code blocks only when explicitly specified
-doctest_test_doctest_blocks = ""
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+autodoc_member_order = "bysource"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# The suffix of source filenames.
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
 source_suffix = ".rst"
 
 # The master toctree document.
@@ -40,53 +59,103 @@ master_doc = "index"
 
 # General information about the project.
 project = "Hip"
-release = version = hip.__version__
 author = hip.__author__
-copyright = "{year}, {author}".format(year=date.today().year, author=author)
+copyright = "%d, %s" % (datetime.date.today().year, author)
+version = hip.__version__
+release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
+# This patterns also effect to html_static_path and html_extra_path
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = "default"
 
-# -- Options for HTML output ---------------------------------------------------
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "alabaster"
+# -- Options for HTML output ----------------------------------------------
+
+# We have to set this ourselves, not only because it's useful for local
+# testing, but also because if we don't then RTD will throw away our
+# html_theme_options.
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "description": "Hip is a new Python HTTP client for everybody",
-    "github_user": "python-trio",
-    "github_repo": "hip",
-    "github_button": False,
-    "github_banner": True,
-    "travis_button": True,
-    "show_powered_by": False,
-    "font_family": "'Roboto', Georgia, sans",
-    "head_font_family": "'Roboto', Georgia, serif",
-    "code_font_family": "'Roboto Mono', 'Consolas', monospace",
+    # default is 2
+    # show deeper nesting in the RTD theme's sidebar TOC
+    # https://stackoverflow.com/questions/27669376/
+    # I'm not 100% sure this actually does anything with our current
+    # versions/settings...
+    "navigation_depth": 4,
+    "logo_only": True,
+    "prev_next_buttons_location": "both",
 }
 
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [alabaster.get_path()]
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
 
-# Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    "**": [
-        "about.html",
-        "navigation.html",
-        "relations.html",
-        "searchbox.html",
-        "donate.html",
-    ]
+
+# -- Options for HTMLHelp output ------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "Hipdoc"
+
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
 }
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.8", None),
-}
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, "Hip.tex", "Hip Documentation", author, "manual"),
+]
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [(master_doc, "hip", "Hip Documentation", [author], 1)]
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (
+        master_doc,
+        "Hip",
+        "Hip Documentation",
+        author,
+        "Hip",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
+]
