7efcb693ebefc37e417339672c3fd987c4a8711f
Quentin Pradet <quentin.pradet@gmail.com>
2020-05-18 20:33:27 +0400

    Asyncify PoolManager redirect tests

--- test/with_dummyserver/async/test_poolmanager.py
+++ test/with_dummyserver/async/test_poolmanager.py
@@ -1,10 +1,13 @@
 import io
+import json
 import pytest
 from ahip import PoolManager, Retry
-from ahip.exceptions import UnrewindableBodyError
+from ahip.exceptions import MaxRetryError, UnrewindableBodyError
 
 from test.with_dummyserver import conftest
 from dummyserver.testcase import HTTPDummyServerTestCase
+
+from test import LONG_TIMEOUT
 
 
 class TestPoolManager(HTTPDummyServerTestCase):
@@ -33,6 +36,193 @@ class TestPoolManager(HTTPDummyServerTestCase):
 
             assert r.status == 200
             assert r.data == b"Dummy server!"
+
+    @conftest.test_all_backends
+    async def test_redirect_twice(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/redirect" % self.base_url},
+                redirect=False,
+            )
+
+            assert r.status == 303
+
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={
+                    "target": "%s/redirect?target=%s/" % (self.base_url, self.base_url)
+                },
+            )
+
+            assert r.status == 200
+            assert r.data == b"Dummy server!"
+
+    @conftest.test_all_backends
+    async def test_redirect_to_relative_url(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "/redirect"},
+                redirect=False,
+            )
+
+            assert r.status == 303
+
+            r = await http.request(
+                "GET", "%s/redirect" % self.base_url, fields={"target": "/redirect"}
+            )
+
+            assert r.status == 200
+            assert r.data == b"Dummy server!"
+
+    @conftest.test_all_backends
+    async def test_cross_host_redirect(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            cross_host_location = "%s/echo?a=b" % self.base_url_alt
+            with pytest.raises(MaxRetryError):
+                await http.request(
+                    "GET",
+                    "%s/redirect" % self.base_url,
+                    fields={"target": cross_host_location},
+                    timeout=LONG_TIMEOUT,
+                    retries=0,
+                )
+
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/echo?a=b" % self.base_url_alt},
+                timeout=LONG_TIMEOUT,
+                retries=1,
+            )
+
+            assert r._pool.host == self.host_alt
+
+    @conftest.test_all_backends
+    async def test_too_many_redirects(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            with pytest.raises(MaxRetryError):
+                await http.request(
+                    "GET",
+                    "%s/redirect" % self.base_url,
+                    fields={
+                        "target": "%s/redirect?target=%s/"
+                        % (self.base_url, self.base_url)
+                    },
+                    retries=1,
+                )
+
+            with pytest.raises(MaxRetryError):
+                await http.request(
+                    "GET",
+                    "%s/redirect" % self.base_url,
+                    fields={
+                        "target": "%s/redirect?target=%s/"
+                        % (self.base_url, self.base_url)
+                    },
+                    retries=Retry(total=None, redirect=1),
+                )
+
+    @conftest.test_all_backends
+    async def test_redirect_cross_host_remove_headers(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/headers" % self.base_url_alt},
+                headers={"Authorization": "foo"},
+            )
+
+            assert r.status == 200
+
+            data = json.loads(r.data.decode("utf-8"))
+
+            assert "Authorization" not in data
+
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/headers" % self.base_url_alt},
+                headers={"authorization": "foo"},
+            )
+
+            assert r.status == 200
+
+            data = json.loads(r.data.decode("utf-8"))
+
+            assert "authorization" not in data
+            assert "Authorization" not in data
+
+    @conftest.test_all_backends
+    async def test_redirect_cross_host_no_remove_headers(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/headers" % self.base_url_alt},
+                headers={"Authorization": "foo"},
+                retries=Retry(remove_headers_on_redirect=[]),
+            )
+
+            assert r.status == 200
+
+            data = json.loads(r.data.decode("utf-8"))
+
+            assert data["Authorization"] == "foo"
+
+    @conftest.test_all_backends
+    async def test_redirect_cross_host_set_removed_headers(
+        self, backend, anyio_backend
+    ):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/headers" % self.base_url_alt},
+                headers={"X-API-Secret": "foo", "Authorization": "bar"},
+                retries=Retry(remove_headers_on_redirect=["X-API-Secret"]),
+            )
+
+            assert r.status == 200
+
+            data = json.loads(r.data.decode("utf-8"))
+
+            assert "X-API-Secret" not in data
+            assert data["Authorization"] == "bar"
+
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": "%s/headers" % self.base_url_alt},
+                headers={"x-api-secret": "foo", "authorization": "bar"},
+                retries=Retry(remove_headers_on_redirect=["X-API-Secret"]),
+            )
+
+            assert r.status == 200
+
+            data = json.loads(r.data.decode("utf-8"))
+
+            assert "x-api-secret" not in data
+            assert "X-API-Secret" not in data
+            assert data["Authorization"] == "bar"
+
+    @conftest.test_all_backends
+    async def test_raise_on_redirect(self, backend, anyio_backend):
+        with PoolManager(backend=backend) as http:
+            r = await http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={
+                    "target": "%s/redirect?target=%s/" % (self.base_url, self.base_url)
+                },
+                retries=Retry(total=None, redirect=1, raise_on_redirect=False),
+            )
+
+            assert r.status == 303
 
 
 class TestFileUploads(HTTPDummyServerTestCase):
